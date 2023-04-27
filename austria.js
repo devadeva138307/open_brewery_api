b1b07ec09a75cc74e921af77288c3035c88e5fa8
devadeva138307 <130572692+devadeva138307@users.noreply.github.com>
2023-04-27 18:32:26 +0530

    Add files via upload

---
+++ austria.js
@@ -0,0 +1,25 @@
+const button = document.querySelector('#austriaButton');
+const table = document.querySelector('#breweriesTable tbody');
+
+button.addEventListener('click', () => {
+  const url = 'https://api.openbrewerydb.org/breweries?by_country=austria';
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      // Build the table rows
+      let rows = '';
+      data.forEach(brewery => {
+        rows += `<tr>
+                    <td>${brewery.name}</td>
+                    <td>${brewery.brewery_type}</td>
+                    <td>${brewery.street}, ${brewery.city}, ${brewery.state}, ${brewery.postal_code}</td>
+                    <td><a href="${brewery.website_url}" target="_blank">${brewery.website_url}</a></td>
+                    <td>${brewery.phone}</td>
+                 </tr>`;
+      });
+      // Add the rows to the table body
+      table.innerHTML = rows;
+    })
+    .catch(error => console.error(error));
+});
+
